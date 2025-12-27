2b7b6e0411ad8862456462007565d9fe564f6fc9
Flayer <apoorvnarkar25@gmail.com>
2025-12-27 01:16:24 -0800

    medway inital welcome

--- app/page.jsx
+++ app/page.jsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <div className="flex min-h-screen justify-center items-center">
-      <h1 className="text-6xl text-white">Hello World !</h1>
+      <h1 className="text-6xl text-white">Medway is live!!!</h1>
     </div>
   );
 }
