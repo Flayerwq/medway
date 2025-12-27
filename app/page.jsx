3e1ce4aa3ac0531f1092f644a4da09383454f4ce
Flayer <apoorvnarkar25@gmail.com>
2025-12-27 01:10:06 -0800

    inital setup

---
+++ app/page.jsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div className="flex min-h-screen justify-center items-center">
+      <h1 className="text-6xl text-white">Hello World !</h1>
+    </div>
+  );
+}
